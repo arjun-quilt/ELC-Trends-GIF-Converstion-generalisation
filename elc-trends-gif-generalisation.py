9ae60dce6eedf30ba82816cc7acd3957e058393f
arjun-quilt <arjun.manikandan@quilt.ai>
2025-04-10 13:44:01 +0530

    update app

--- elc-trends-gif-generalisation.py
+++ elc-trends-gif-generalisation.py
@@ -25,16 +25,6 @@ import nest_asyncio
 import subprocess
 
 # Install system dependencies
-try:
-    subprocess.run([
-        "apt-get", "install", "-y",
-        "libnss3", "libnspr4", "libatk1.0-0", "libatk-bridge2.0-0", "libatspi2.0-0",
-        "libxcomposite1", "libxdamage1", "libxfixes3", "libxrandr2",
-        "libgbm1", "libxkbcommon0", "libasound2"
-    ], check=True)
-except Exception as e:
-    print(f"Error installing system dependencies: {e}")
-
 # Run the Playwright install command
 try:
     subprocess.run(["playwright", "install"], check=True)
